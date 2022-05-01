fa6188ddb5f14dcb753358ffa428460f52d13c1e
mnnkkk <46259595+mnnkkk@users.noreply.github.com>
2022-05-01 11:48:17 +0800

    Refactor

--- src/router/index.js
+++ src/router/index.js
@@ -13,14 +13,6 @@ const routes = [
     redirect: '/app-service/app-service-list'
   },
   {
-    path: '/app-service/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
     path: '/app-service/app-service-list',
     name: 'app-service-list',
     // route level code-splitting
